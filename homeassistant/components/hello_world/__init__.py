df5bafbf7a03f8558892b5f11faa6cecd53089d2
Liafonx <liafonx@outlook.com>
2025-09-15 21:31:55 +0000

    Hello World

--- homeassistant/components/hello_world/__init__.py
+++ homeassistant/components/hello_world/__init__.py
@@ -6,10 +6,10 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 
-async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry) -> bool:
+async def async_setup(hass: HomeAssistant, config: ConfigEntry) -> bool:
     """Set up hello from a config entry."""
 
-    hass.states.set("hello_world.hello", "Hello, world!")
+    hass.states.async_set("hello_world.hello", "Hello, world!")
 
     # Return boolean to indicate that initialization was successful.
     return True
